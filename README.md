e4616a0a05a8c2a760ccbb185962df95140d903e
MonadWizard <monad.wizard.r@gmail.com>
2021-08-26 17:26:02 +0600

    0.02_mixin

---
+++ README.md
@@ -0,0 +1,21 @@
+mixin simplify our work and implement as dry(don't repeate yourself) principals. 😏
+
+Mixins:
+
+    ListModelMixin
+    CreateModelMixin
+    RetrieveModelMixin
+    UpdateModelMixin
+    DestroyModelMixin
+
+
+Mixin provided us
+
+| Action methods | mixin Classes      | Handler Methods |
+|----------------|--------------------|-----------------|
+| list()         | ListModelMixin     | get()           |
+| create()       | CreateModelMixin   | post()          |
+| retrieve()     | RetrieveModelMixin | get()           |
+| update()       | UpdateModelMixin   | put()           |
+| destroy()      | DestroyModelMixin  | delete()        |
+
--- mixins/cbvApp/views.py
+++ mixins/cbvApp/views.py
@@ -3,53 +3,37 @@ from django.shortcuts import render
 from cbvApp.models import Student
 from cbvApp.serializers import StudentSerializer
 
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
-from django.http import Http404
+from rest_framework import generics,mixins
 
 
+# just call and use handler method
 
-# Create your views here.
-class StudentList(APIView):
-
+# non-primery key based operations
+class StudentList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class= StudentSerializer
+    
     def get(self,request):
-        students = Student.objects.all()
-        serializer = StudentSerializer(students,many=True)
-        return Response(serializer.data)
-
+        return self.list(request)
+    
     def post(self,request):
-        serializer = StudentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        return self.create(request)
 
-class StudentDetail(APIView):
-    def get_object(self,pk):
-        try:
-            return Student.objects.get(pk=pk)
-        except Student.DoesNotExist:
-            raise Http404
+
+# primery key based operations
+
+class StudentDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class= StudentSerializer
 
     def get(self,request,pk):
-        student = self.get_object(pk)
-        serializer = StudentSerializer(student)
-        return Response(serializer.data)
-
+        return self.retrieve(request,pk)
+    
     def put(self,request,pk):
-        student = self.get_object(pk)
-        serializer = StudentSerializer(student,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
+        return self.update(request,pk)
+    
     def delete(self,request,pk):
-        student = self.get_object(pk)
-        student.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
+        return self.destroy(request,pk)
 
 
 
