0bf5566974e57bebdccb2232d0410b27864ac3f5
MonadWizard <monad.wizard.r@gmail.com>
2021-08-26 18:12:25 +0600

    0.02_generic_views

--- generic_views/cbvApp/views.py
+++ generic_views/cbvApp/views.py
@@ -1,40 +1,20 @@
-from django.shortcuts import render
-
 from cbvApp.models import Student
 from cbvApp.serializers import StudentSerializer
 
-from rest_framework import generics,mixins
+from rest_framework import generics
 
 
 # just call and use handler method
 
 # non-primery key based operations
-class StudentList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+class StudentList(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class= StudentSerializer
-    
-    def get(self,request):
-        return self.list(request)
-    
-    def post(self,request):
-        return self.create(request)
 
 
 # primery key based operations
-
-class StudentDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class= StudentSerializer
-
-    def get(self,request,pk):
-        return self.retrieve(request,pk)
-    
-    def put(self,request,pk):
-        return self.update(request,pk)
-    
-    def delete(self,request,pk):
-        return self.destroy(request,pk)
-
-
 
 
